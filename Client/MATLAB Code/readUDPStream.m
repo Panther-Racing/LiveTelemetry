aa2914a975f5d354c3fc266a077309dd813f3d60
RaheelFarouk <raheelfarouk@gmail.com>
2022-10-08 16:50:12 -0400

    update readUDPStream

--- Client/MATLAB Code/readUDPStream.m
+++ Client/MATLAB Code/readUDPStream.m
@@ -3,4 +3,6 @@ udpSocket = udpport("byte", "IPV4");
 % write to UDP Server
 write(udpSocket, "I want data", "string", "raheelfarouk.tplinkdns.com", 20001);
 
-udpData = read(udpSocket, 100, "string")
+while true
+    udpData = readline(udpSocket)
+end
