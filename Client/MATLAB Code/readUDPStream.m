d3ad2b9a3b02d1ff5fd2520286b383c5978139a1
Panther Racing (MEMS-FSAE-3) <pitt.fsae@gmail.com>
2022-10-09 18:29:01 -0400

    Changed matlab code to send a tcp request to server

--- Client/MATLAB Code/readUDPStream.m
+++ Client/MATLAB Code/readUDPStream.m
@@ -1,7 +1,9 @@
 udpSocket = udpport("byte", "IPV4");
+client = tcpclient("raheelfarouk.tplinkdns.com",20003)
 
 % write to UDP Server
-write(udpSocket, "I want data", "string", "raheelfarouk.tplinkdns.com", 20001);
+%write(udpSocket, "I want data", "string", "raheelfarouk.tplinkdns.com", 20002);
+write(client,"Add Me","string")
 
 while true
     udpData = readline(udpSocket)
