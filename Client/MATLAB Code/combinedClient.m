bbdb84b86e8b7704029269e3e54f26f1d122024d
fjl111 <fjl11@pitt.edu>
2022-10-13 16:33:46 -0400

    Update combinedClient.m
    Rearranged the code for some parts to be in the while loop and setup parts to be before the while loop.

--- Client/MATLAB Code/combinedClient.m
+++ Client/MATLAB Code/combinedClient.m
@@ -1,3 +1,6 @@
+clear
+clc
+
 %create Virtual CAN Bus channel
 can1 = canChannel('MathWorks', 'Virtual 1', 1)
 
@@ -7,27 +10,27 @@ configBusSpeed(can1,1000000)
 %This is the message object we are sending to the CAN Bus -- canMessage(CAN ID, Extended, Datalength)
 messageout = canMessage(500,false,8)
 
-%This makes the message to send --
-%pack(message,value,startbit,signalsize,byteorder) 
-pack(messageout,25,0,8,'BigEndian')
-
-%Set the CAN Channel online
-start(can1)
-
-%Send the message, messageout has the data to send
-transmit(can1,messageout)
-  
 %UDP Socket object
 udpSocket1 = udpport("byte", "IPV4");
-
+    
 %udpSocket2 = udpport("byte", "IPV4");
 %client = tcpclient("raheelfarouk.tplinkdns.com",20003)
-
+    
 % write to UDP Server
 write(udpSocket1, "Add Me", "string", "raheelfarouk.tplinkdns.com", 20003);
 %write(client,"Add Me")
 
-%Read data from UDP Server
 while true
+    %Read data from UDP Server
     udpData = readline(udpSocket1)
+
+    %This makes the message to send --
+    %pack(message,value,startbit,signalsize,byteorder) 
+    pack(messageout,25,0,8,udpData)
+    
+    %Set the CAN Channel online
+    start(can1)
+    
+    %Send the message, messageout has the data to send
+    transmit(can1,messageout)
 end
