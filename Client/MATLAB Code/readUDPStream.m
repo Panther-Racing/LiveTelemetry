a4d7fae69a1312e171f5d8fd454c741fbd66689d
Raheel Farouk <raheelfarouk@gmail.com>
2022-10-13 15:18:51 -0400

    Added Comments to MATLAB Code

--- Client/MATLAB Code/readUDPStream.m
+++ Client/MATLAB Code/readUDPStream.m
@@ -1,11 +1,14 @@
+%UDP Socket object
 udpSocket1 = udpport("byte", "IPV4");
-udpSocket2 = udpport("byte", "IPV4");
+
+%udpSocket2 = udpport("byte", "IPV4");
 %client = tcpclient("raheelfarouk.tplinkdns.com",20003)
 
 % write to UDP Server
 write(udpSocket1, "Add Me", "string", "raheelfarouk.tplinkdns.com", 20003);
 %write(client,"Add Me")
 
+%Read data from UDP Server
 while true
     udpData = readline(udpSocket1)
 end
--- Client/MATLAB Code/virtualCANBusAdapter.m
+++ Client/MATLAB Code/virtualCANBusAdapter.m
@@ -7,11 +7,12 @@ configBusSpeed(can1,1000000)
 %This is the message object we are sending to the CAN Bus -- canMessage(CAN ID, Extended, Datalength)
 messageout = canMessage(500,false,8)
 
-%This makes the message to send -- pack(message,value,startbit,signalsize,byteorder)
+%This makes the message to send --
+%pack(message,value,startbit,signalsize,byteorder) 
 pack(messageout,25,0,8,'BigEndian')
 
 %Set the CAN Channel online
 start(can1)
 
-%Send the message
+%Send the message, messageout has the data to send
 transmit(can1,messageout)
