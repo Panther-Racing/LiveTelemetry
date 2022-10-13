ee64329fa8682578d9847f4e56ea568ee976e901
fjl111 <fjl11@pitt.edu>
2022-10-13 16:09:14 -0400

    Cleaned up code by removing unused sockets and adding more comments.

--- Server/udpServerRecieveAndSend.py
+++ Server/udpServerRecieveAndSend.py
@@ -3,9 +3,8 @@
 import socket
 import selectors
 import types
-import time
 
-# Create a selector for handling multiple clients
+# Create a selector for handling data recieve events
 sel = selectors.DefaultSelector()
 
 # Change to local IP of server -Future version get this automatically
@@ -13,42 +12,37 @@ localIP = "192.168.0.150"
 # Car UDP Port
 carPort = 20001
 # Client UDP Port
-clientPort = 20002
-# Create a UDP Port to listen for new client requests
-listenPort = 20003
+clientPort = 20003
 # UDP Buffer size maybe? Need to check CHK
 bufferSize = 1024
-
-# Message to send to deivce, maybe a pairing message to recieve car number? FUT
-msgFromServer = "Hello UDP Client"
-bytesToSend = str.encode(msgFromServer)
 
 # Create a list of client addresses to send to
 clientList = []
 
-# Create 3 datagram sockets, one for car, one for sending to clients, and one for adding new clients
-UDPCarSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-listenSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Create 2 datagram sockets, one for car, one for listening for and sending to clients
+carSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+clientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # Bind all sockets to address and ip
-UDPCarSocket.bind((localIP, carPort))
-UDPClientSocket.bind((localIP, clientPort))
-listenSocket.bind((localIP, listenPort))
+carSocket.bind((localIP, carPort))
+clientSocket.bind((localIP, clientPort))
 
-# Add listening socket to selector so it creates an interrupt when it recieves data
-sel.register(listenSocket, selectors.EVENT_READ, data=None)
+# Add client socket to selector so an interrupt event is crated when a client tries to join
+#Don't create a data attribute so that it can be told apart from the car socket
+sel.register(clientSocket, selectors.EVENT_READ, data=None)
 
-# Add the car socket to the selector
+#Create a data attribute to differentiate car events from client events and to log incoming car data
 carData = types.SimpleNamespace(type="car", inb=b"")
-sel.register(UDPCarSocket, selectors.EVENT_READ, data=carData)
+# Add the car socket to the selector so incoming car data creates an interrupt event
+sel.register(carSocket, selectors.EVENT_READ, data=carData)
+
 
 print("UDP server up and listening")
 
 
-# Function to get new clients
+# Function to get new clients -- called when clients request to be added
 def accept_wrapper(sock):
-    # Get message and address from listenSocket
+    # Get message and address from the clientSocket
     newClient = sock.recvfrom(bufferSize)
     # If client request is valid, add client to clientList, else produce error
     if newClient[0] == b'Add Me':  # Message that comes from UDP comes in as a byte
@@ -59,16 +53,16 @@ def accept_wrapper(sock):
         print(f"Client Said: {newClient[0]}")
 
 
-# Function to
+# Function to receive data from car and send it to clients -- called when new car data is received
 def data_handler(key):
     # Get incoming data from the car
-    carDataPackage = UDPCarSocket.recvfrom(bufferSize)
+    carDataPackage = carSocket.recvfrom(bufferSize)
 
-    # Extract Message from the car
+    # Extract Message from the car data
     carMsg = carDataPackage[0]
     print("Car Data:{}".format(carMsg))
 
-    # Add notes for the socket
+    # Log received data in notes connected to the car socket
     data = key.data
     data.inb += carMsg
 
@@ -76,32 +70,35 @@ def data_handler(key):
     carMsgString = carMsg.decode("utf-8")
     carMsgString += "\n"
 
-    # Encode the car data
+    # Encode the car data into bytes
     bytesToSend = str.encode(carMsgString)
 
+    #For each client, send out the received car data
     for clientAddress in clientList:
-        # Sending data to client
-        listenSocket.sendto(bytesToSend, clientAddress)
+        clientSocket.sendto(bytesToSend, clientAddress)
 
 
 # Main program
 try:
-    # Listen for incoming datagrams
+    # Run indefinitely to constantly listen for client requests and car data
     while (True):
-
-        # Wait for an event (receiving input from or ready to send output to a socket)
+        # Wait for an event (input has been received on one of the sockets) and never timeout
         events = sel.select(timeout=None)
-        # Extract key, which holds the socket object and mask which is an event mask of the operations that are ready
+        # Extract key, which holds the socket object that triggered the event, and mask, which is an event mask
+        # of the operations that are ready (if it is a receive or send event - we only use receive events)
         for key, mask in events:
-            # If key.data is none, then the key is the listenPort and we'll call a function to add it to the client list
+            # If key.data is none, then the key is the client (which has no data label)
+            # and we'll call a function to add it to the client list
             if key.data is None:
                 print("client request received")
                 accept_wrapper(key.fileobj)
-            # If key.data is not none, then the event is from the car so run a function to recieve and send out that data
+            # If key.data is not none, then the event is from the car so run a function to receive and send out that data
             else:
                 data_handler(key)
 
 except KeyboardInterrupt:
+    #If a user on the server interrupts the program, it will stop the infinite loop
     print("Caught Keyboard interrupt, exiting")
 finally:
+    #The selector will be closed when the program ends
     sel.close()
