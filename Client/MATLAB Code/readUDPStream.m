a2b0759e0ab74e7a4e118d91bb745194aa95e938
Raheel Farouk <raheelfarouk@gmail.com>
2022-10-15 16:41:41 -0400

    fix conflict

--- Client/MATLAB Code/readUDPStream.m
+++ Client/MATLAB Code/readUDPStream.m
@@ -5,7 +5,7 @@ udpSocket1 = udpport("byte", "IPV4");
 %client = tcpclient("raheelfarouk.tplinkdns.com",20003)
 
 % write to UDP Server
-write(udpSocket1, "Add Me", "string", "raheelfarouk.tplinkdns.com", 20003);
+write(udpSocket1, "Add Me", "string", "192.168.1.100", 20003);
 %write(client,"Add Me")
 
 %Read data from UDP Server
