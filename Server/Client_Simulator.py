b3ec9e6c71fb1d6613ccc184e46ecd55bde2f5f5
fjl111 <115193218+fjl111@users.noreply.github.com>
2022-11-14 22:44:36 -0500

    Rewrote Client simulator to run more streamlined successive tests (considers repeated numbers repeats instead of 9 drops now and counts the time it takes to receive the packages)

--- Server/Client_Simulator.py
+++ Server/Client_Simulator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import socket
+import time
 
 # Get the address of the test computer and the server computer
 myPort = input("Enter the IP Address of this Computer: ")
@@ -12,49 +13,61 @@ mainSocket.bind((myPort, 20003))
 bytesToSend = str.encode("Add Me")
 mainSocket.sendto(bytesToSend, (serverPort, 20003))
 
-# Initialize variables to track how many messages are being missed/corrupted
-missed = 0
-received = 0
-past = 0
-corrupted = 0
+for i in range(10):
 
-while received < 2000:
-    message = mainSocket.recv(1024).decode()
-    print(message)
+    # Initialize variables to track how many messages are being missed/corrupted
+    missed = 0
+    received = 0
+    past = 0
+    corrupted = 0
+    repeated = 0
+    startTime = time.time()
 
-    # Increment variable tracking the number of messages received
-    received += 1
+    while received < 2000:
+        message = mainSocket.recv(1024).decode()
+        print(message)
 
-    # Find the difference between the past and current message
-    difference = int(message)
-    difference = difference - past
-    print(f"Past: {past}       Difference: {difference}")
+        # Increment variable tracking the number of messages received
+        received += 1
 
-    # If the difference is greater than 9 than it must be due to corruption (The counter is only 0 to 9)
-    if abs(difference) > 9:
-        corrupted += 1
-        print("corrupted")
-    # If the difference is negative, count the missing packages by adding 9 to the difference
-    elif difference < 0:
-        missed += difference + 9
-        print(f"missed {difference+9}")
-    # If the number received is the same twice in a row then 16 packets were missed
-    elif difference == 0:
-        missed += 9
-        print(f"missed 9")
-    # Otherwise the difference is positive which means it is one more than the number of packages missed
-    else:
-        missed += difference - 1
-        print(f"missed {difference-1}")
+        # Find the difference between the past and current message
+        difference = int(message)
+        difference = difference - past
+        print(f"Past: {past}       Difference: {difference}")
 
-    past = int(message)
-    print()
-    print()
-    print()
+        # If the difference is greater than 9 than it must be due to corruption (The counter is only 0 to 9)
+        if abs(difference) > 9:
+            corrupted += 1
+            print("corrupted")
+        # If the difference is negative, count the missing packages by adding 9 to the difference
+        elif difference < 0:
+            missed += difference + 9
+            print(f"missed {difference+9}")
+        # If the number received is the same twice in a row then 16 packets were missed
+        elif difference == 0:
+            # missed += 9
+            repeated += 1
+            received -= 1
+            print(f"repeated")
+        # Otherwise the difference is positive which means it is one more than the number of packages missed
+        else:
+            missed += difference - 1
+            print(f"missed {difference-1}")
 
-# Account for the first time past not matching difference because past is initialized to 0
-corrupted -= 1
+        past = int(message)
+        print()
+        print()
+        print()
 
-percentMissed = (missed / (missed+received)) * 100
-print(f"Percent of Messages Missed: {percentMissed}%")
-print(f"Number of Messages corrupted: {corrupted}")
+    # Account for the first time past not matching difference because past is initialized to 0
+    corrupted -= 1
+    endTime = time.time()
+
+    percentMissed = (missed / (missed+received)) * 100
+    print(f"Run {i}")
+    print(f"Percent of Messages Missed: {percentMissed}%")
+    print(f"Number of Messages corrupted: {corrupted}")
+    print(f"Number of times reread packets: {repeated}")
+    print(f"Number of Messages Received: {received}")
+    print(f"Time to run: {endTime-startTime} seconds")
+    time.sleep(10)
