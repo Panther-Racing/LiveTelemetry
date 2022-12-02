990aebcaee0a70a6ade8d199462f337ca1ebb554
fjl111 <fjl11@pitt.edu>
2022-12-01 22:13:48 -0500

    Added DBC Reading Ability
    Added 2 DBC's and the ability to add them to the can database

--- Server/CAN_Translate.py
+++ Server/CAN_Translate.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
-import os
-from binascii import hexlify
+# import os
+# from binascii import hexlify
 import can
 import cantools
-from pprint import pprint
+# from pprint import pprint
+# import can_decoder
+
+# db = can_decoder.load_dbc('DBC2.dbc')
+# decoder = can_decoder.DataFrameDecoder(db)
+
+# can.rc['interface'] = 'socketcan'
+# can.rc['channel'] = 'vcan0'
+# can_bus = can.interface.Bus()
+
 
 db = cantools.database.Database()
-db.add_dbc_file('fileName.dbc')
-
-can_bus = can.interface.Bus('')
+db.add_dbc_file('DBC1.dbc')
