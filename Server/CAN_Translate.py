5f0cd495ab20e234b9a337509cacf3be5f4d4825
fjl111 <fjl11@pitt.edu>
2022-12-01 20:40:49 -0500

    Began Writing CAN Translater

--- Server/CAN_Translate.py
+++ Server/CAN_Translate.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+import os
+from binascii import hexlify
+import can
+import cantools
+from pprint import pprint
+
+db = cantools.database.Database()
+db.add_dbc_file('fileName.dbc')
+
+can_bus = can.interface.Bus('')
