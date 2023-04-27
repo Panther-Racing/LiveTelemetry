feb1271ecfde3ca69c8703a0ab7e483909870a2e
fjl111 <115193218+fjl111@users.noreply.github.com>
2023-04-26 20:00:47 -0400

    Send JSON to SON to SQL Converter
    Created a program that sends JSON to the program that translates JSON into SQL. Modified them to connect better. Full system roughly complete!

---
+++ Server/CAN_Translater/CAN_Translate_And_Send.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+
+import socket
+import selectors
+import cantools
+import json
+import time
+import requests
+import sys
+
+# import can_decoder
+
+# db = can_decoder.load_dbc('DBC2.dbc')
+# decoder = can_decoder.DataFrameDecoder(db)
+
+# can.rc['interface'] = 'socketcan'
+# can.rc['channel'] = 'vcan0'
+# can_bus = can.interface.Bus()
+
+sel = None
+db = None
+bufferSize = 1024
+CANSocket = None
+serverIP = None
+receivePort = None
+outfile = open('output.txt', 'a')
+errorfile = open('error.txt', 'a')
+missing_CAN_file = open('missing_CAN.txt', 'a')
+missing_CAN = set()
+nonLiterals = set()
+nonLiteral_file = open('nonLiterals.txt', 'a')
+# jsonFile = open('forjson.txt', 'a')
+json_file_name = 'json_data.json'
+push_url = 'https://api.powerbi.com/beta/9ef9f489-e0a0-4eeb-87cc-3a526112fd0d/datasets/b55e639b-08c8-4715-95a4-d4e2aae6e3a0/rows?key=aiW69d9hteGIf8gv%2Fe9JNAFJPr9W%2FjSy%2FHyu%2B0EOh08%2F3lTvQ1Yp53mZv1go6%2BiDu90U37Z31oDj8iL5pLnohA%3D%3D'
+test = 0
+
+# Setup socket to send JSON to SQL program
+port = 1500
+address = "127.0.0.1"
+
+try:
+    json_sock = socket.socket()
+    json_sock.connect((address, port))
+except socket.error as err:
+    print('Socket error because of %s' % (err))
+
+
+
+def setup():
+    global sel
+    global db
+    global CANSocket
+    global serverIP
+    global receivePort
+    global outfile
+    # Connect to server to get CAN data
+    # Get the address of the computer, what port to be on, and the IP address of the server
+    localIP = input('Enter the IP address of this program: ')
+    receivePort = 20003
+    # serverIP = input('Enter the IP address of the server: ')
+    serverIP = socket.gethostbyname('litelserver.eastus.cloudapp.azure.com')
+    # Create the socket
+    CANSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    CANSocket.bind((localIP, receivePort))
+    # Request to be added to the server
+    bytesToSend = str.encode('Add Me')
+    CANSocket.sendto(bytesToSend, (serverIP, receivePort))
+    outfile.write(f'Added as client on {serverIP}\n')
+    outfile.write(f'UDP server up and listening at {localIP} on port {receivePort}\n')
+    # print(f'Added as client on {serverIP}')
+    # print(f'UDP server up and listening at {localIP} on port {receivePort}')
+
+    # Create a selector for handling data receive events
+    sel = selectors.DefaultSelector()
+    sel.register(CANSocket, selectors.EVENT_READ, data=None)
+    outfile.write('Selector created\n')
+    # print('Selector created')
+
+    # Add the DBC file to the CAN reader
+    db = cantools.database.Database()
+    db.add_dbc_file('DBCS/DBC1.dbc')
+
+    # Reset the json file
+    json_file = open(json_file_name, 'w')
+    json_file.write('{}')
+
+
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
+
+
+# Translate each string from the decoded CAN message into a dictionary and then output that dictionary to the json file
+def to_json(message):
+    with open(json_file_name, 'r+') as json_file:
+        # # Traverse through the new dictionary and add time stamps
+        # for key in message:
+        #     data_value = message[key]
+        #     message[key] = (time.time(), data_value)
+        json_dict = json.load(json_file)
+        # print(json_dict)
+        json_dict.update(message)
+        #Add a value to hold the current time
+        json_dict.update({'Timestamp': time.time()})
+        json_file.close()
+        open(json_file_name, 'w').close()
+        json_file = open(json_file_name, 'r+')
+        json.dump(json_dict, json_file)
+        send_json(json_dict)
+
+
+def data_handler(key):
+    global db
+    global outfile
+    global errorfile
+    # Extract the message from the socket
+    message = key.fileobj.recvfrom(bufferSize)[0].decode().strip()
+
+    # Separate CAN message into id and data
+    # Except non hexadecimal values
+    try:
+        frame_id = int(message[find_nth(message, ',', 1)+1:find_nth(message, ',', 2)], 16)
+    except ValueError as error:
+        # print('Non hexadecimal frame_id: %s' % error)
+        outfile.write('Non hexadecimal frame_id: %s\n\n' % error)
+        errorfile.write('Non hexadecimal frame_id: %s\n\n' % error)
+        nonLiterals.add(str(error))
+        frame_id = 'ERROR'
+
+    data = bytes(message[find_nth(message, ',', 3)+1:], 'utf-8')
+    # print(data)
+    outfile.write(f'Frame ID: {frame_id}     data: {data}\n\n')
+    # print(f'Frame ID: {frame_id}     data: {data}')
+
+    try:
+        # Decode each incoming message and print it out
+        # outfile.write(str(db.decode_message(frame_id, data)))
+        # jsonFile.write(str(db.decode_message(frame_id, data)))
+        to_json(db.decode_message(frame_id, data))
+        # jsonFile.write('\n')
+        outfile.write('\n\n')
+        print(db.decode_message(frame_id, data))
+    except KeyError as error:
+        outfile.write('Key error: %s\n\n' % error)
+        errorfile.write('Key error: %s\n\n' % error)
+        missing_CAN.add(str(error))
+        # print('Key error: %s' % error)
+    except ValueError as error:
+        print(error)
+
+
+def send_json(json_string):
+    json_result = json.dumps(json_string)
+
+    try:
+        json_sock.send(json_result.encode())
+    except ConnectionResetError as error:
+        print(error)
+
+    print(json_result, 'was sent!')
+    time.sleep(1)
+
+
+def main():
+    global outfile
+    global errorfile
+    global missing_CAN_file
+    global nonLiteral_file
+    outfile.write('NEW INSTANCE')
+    outfile.write('-------------------------------------------------------\n\n\n\n')
+    errorfile.write('NEW INSTANCE')
+    errorfile.write('-------------------------------------------------------\n\n\n\n')
+    missing_CAN_file.write('NEW INSTANCE')
+    missing_CAN_file.write('-------------------------------------------------------\n\n\n\n')
+    nonLiteral_file.write('NEW INSTANCE')
+    nonLiteral_file.write('-------------------------------------------------------\n\n\n\n')
+    setup()
+    while True:
+        # Wait for an event (input has been received on one of the sockets) and never timeout
+        events = sel.select(timeout=None)
+
+        # Call a function to handle data
+        for key, mask in events:
+            data_handler(key)
+
+
+try:
+    main()
+except KeyboardInterrupt:
+    # When program is terminated (keyboard interrupt) ask to be removed from the server
+    bytesToSend = str.encode('Remove Me')
+    CANSocket.sendto(bytesToSend, (serverIP, receivePort))
+    print(f'Removed from {serverIP}')
+    outfile.write(f'Removed from {serverIP}\n')
+    missing_CAN_file.write(str(missing_CAN))
+    nonLiteral_file.write(str(nonLiterals))
+    json_sock.close()
--- Server/Send_SQL/send_sql.py
+++ Server/Send_SQL/send_sql.py
@@ -1,5 +1,7 @@
 import pyodbc
 import json
+import socket
+import time
 
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
@@ -12,34 +14,60 @@ password = 'li.telServer;'
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password+'TrustServerCertificate=yes;')
 cursor = conn.cursor()
 
+sock = socket.socket()
+print("Socket created ...")
+
+port = 1500
+sock.bind(('', port))
+sock.listen(5)
+
+print('socket is listening')
+
+c, addr = sock.accept()
+print('got connection from ', addr)
+
+while True:
+    json_received = c.recv(1024)
+    print('Json received -->', json_received.decode().replace('\\', ''))
+    try:
+        # Load the JSON data from the string
+        json_data = json.loads(json_received)
+    except json.decoder.JSONDecodeError as error:
+        print('ERROR1', error)
+
+    # Define the SQL query
+    sql_query = "INSERT INTO dbo.race_data ({}) VALUES ({})"
+
+    # Build the list of column names and values for the SQL query
+    column_names = []
+    column_values = []
+
+    for key, value in json_data.items():
+        column_names.append(key)
+        column_values.append(value)
+
+    # Build the placeholders for the SQL query
+    placeholders = ', '.join(['?'] * len(column_names))
+
+    try:
+        # Insert the variables and their values into the database using the Pyodbc cursor
+        cursor = conn.cursor()
+        cursor.execute(sql_query.format(', '.join(column_names), placeholders), column_values)
+        conn.commit()
+    except pyodbc.ProgrammingError as error:
+        print('ERROR2', error)
+
+    # Close the database connection
+    # conn.close()
+
+    print('Sent')
+
+    time.sleep(1)
+
+    # c.close()
+
 # Test JSON String
-json_string = '{"Differential_Temperature_Front": 2.1, "Differential_Temperature_Centre": 2.3, "Differential_Temperature_Rear": 5.4}'
-
-# Load the JSON data from the string
-json_data = json.loads(json_string)
-
-# Define the SQL query
-sql_query = "INSERT INTO dbo.race_data ({}) VALUES ({})"
-
-# Build the list of column names and values for the SQL query
-column_names = []
-column_values = []
-
-for key, value in json_data.items():
-    column_names.append(key)
-    column_values.append(value)
-
-# Build the placeholders for the SQL query
-placeholders = ', '.join(['?'] * len(column_names))
-
-# Insert the variables and their values into the database using the Pyodbc cursor
-cursor = conn.cursor()
-cursor.execute(sql_query.format(', '.join(column_names), placeholders), column_values)
-conn.commit()
-
-# Close the database connection
-conn.close()
+# json_string = '{"Differential_Temperature_Front": 2.1, "Differential_Temperature_Centre": 2.3, "Differential_Temperature_Rear": 5.4}'
 
 
-print('Sent')
 
