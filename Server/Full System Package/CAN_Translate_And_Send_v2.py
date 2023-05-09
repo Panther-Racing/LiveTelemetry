20e49dbaa113a7686999218cad14c38bb150a960
fjl111 <fjl11@pitt.edu>
2023-05-09 18:33:38 +0200

    Stopped removing spaces in new can translate

--- Server/Full System Package/CAN_Translate_And_Send_v2.py
+++ Server/Full System Package/CAN_Translate_And_Send_v2.py
@@ -79,7 +79,7 @@ def data_handler(data):
     print(f'Frame ID: {frame_id}     data: {data}')
 
     data_string = message[find_nth(message, ',', 2)+1:]
-    data_string = data_string.replace(' ', '')
+    # data_string = data_string.replace(' ', '')
     data = bytes(data_string, 'utf-8')
 
     reversed_data = reverse(data_string)
