d4436f7f520f7778bc0faa939e7944e7e074d0af
fjl111 <fjl11@pitt.edu>
2023-05-09 19:09:51 +0200

    Defined string encoding

--- Server/Full System Package/CAN_Translate_And_Send_v2.py
+++ Server/Full System Package/CAN_Translate_And_Send_v2.py
@@ -112,7 +112,7 @@ def data_handler(data):
     print(data_bytes)
 
     formatted_data = bytes(frame_byte1 + frame_byte2 + data_bytes[0] + data_bytes[1] + data_bytes[2] + data_bytes[3] +
-                           data_bytes[4] + data_bytes[5] + data_bytes[6] + data_bytes[7])
+                           data_bytes[4] + data_bytes[5] + data_bytes[6] + data_bytes[7], 'utf-8')
 
     try:
         # Decode each incoming message
