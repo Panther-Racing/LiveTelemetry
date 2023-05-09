f815b64dd3efcf1e4a19a64ed5bb902f37a297b0
fjl111 <fjl11@pitt.edu>
2023-05-09 23:05:15 +0200

    Removing prints

--- Server/Full System Package/CAN_Translate_And_Send_v1.py
+++ Server/Full System Package/CAN_Translate_And_Send_v1.py
@@ -88,20 +88,20 @@ def data_handler(data):
     data_reformatted = ''
     while next_space >= 0:
         next_space = find_nth(data_string, ' ', i + 1)
-        print(next_space)
-        print(next_space-current_space)
+        # print(next_space)
+        # print(next_space-current_space)
         if next_space - current_space == 1:
             data_reformatted += ('\\x0' + data_string[start_pos:next_space])
         elif next_space - current_space == 2:
             data_reformatted += ('\\x' + data_string[start_pos:next_space])
         else:
             data_reformatted += ('\\x' + data_string[start_pos:])
-        print('looking for more spaces')
+        # print('looking for more spaces')
         i += 1
         current_space = next_space + 1
         start_pos = current_space
 
-    print(data_reformatted)
+    # print(data_reformatted)
     fixed_data = bytes(data_reformatted, 'utf-8')
     print(fixed_data)
 
@@ -110,7 +110,7 @@ def data_handler(data):
 
     try:
         # Decode each incoming message
-        to_json(db.decode_message(frame_id_or_name=frame_id, data=fixed_data, decode_choices=False, scaling=True, decode_containers=False, allow_truncated=True))
+        to_json(db.decode_message(frame_id_or_name=frame_id, data=fixed_data, decode_choices=False, scaling=True, decode_containers=False, allow_truncated=False))
     except KeyError as error:
         print('Key error: %s' % error)
     except ValueError as error:
