74ce494a0f3c0608b549c7d43bafc3fad2e70764
fjl111 <115193218+fjl111@users.noreply.github.com>
2024-02-07 00:19:37 -0500

    Test Logs

--- Server/Full System Package/Server_v4.py
+++ Server/Full System Package/Server_v4.py
@@ -11,7 +11,8 @@ def setup_server():
     # localIP = input("Enter the IP Address of the server: ")
     # Use IP address of current computer
     # localIP = socket.gethostbyname(socket.gethostname())
-    localIP = '192.168.88.251'
+    localIP = '192.168.88.250'
+    # localIP = '192.168.88.251'
     # localIP = '127.0.0.1'
     print(localIP, "Is being used")
 
