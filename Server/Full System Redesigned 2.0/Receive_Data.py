8bd3fd6c19f80e87b0fe93608aa093c0c2aed88c
Frederick Laudati <fjl11@pitt.edu>
2024-05-31 00:38:10 -0400

    Change how data is received

--- Server/Full System Redesigned 2.0/Receive_Data.py
+++ Server/Full System Redesigned 2.0/Receive_Data.py
@@ -7,11 +7,12 @@ async def begin(raw_data_queue, terminate_event):
     s.settimeout(1)
     print(f'Listening at {socket.gethostbyname(socket.gethostname())} on port 20001')
 
-    loop = asyncio.get_running_loop()
+    # loop = asyncio.get_running_loop()
 
     while not terminate_event.is_set():
         try:
-            data, addr = await loop.run_in_executor(None, s.recvfrom, 1024)
+            # data, addr = await loop.run_in_executor(None, s.recvfrom, 1024)
+            data = s.recvfrom(1024)
             print(f'Received {data}')
             await raw_data_queue.put(data)
         except socket.timeout:
