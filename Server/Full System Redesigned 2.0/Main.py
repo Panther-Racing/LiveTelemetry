f5296f0d4c6549c37adb9b4cecea40b0169c0512
Frederick Laudati <fjl11@pitt.edu>
2024-05-31 00:05:32 -0400

    Try using all asyncio and no threads

--- Server/Full System Redesigned 2.0/Main.py
+++ Server/Full System Redesigned 2.0/Main.py
@@ -14,8 +14,10 @@ terminate_event = asyncio.Event()
 # Load the CAN database
 db = cantools.database.load_file('DBCS/Combined.dbc')
 
+
 def data_receiver():
     Receive_Data.begin(raw_data_queue, terminate_event)
+
 
 async def data_translator():
     translator = CAN_Translate.CANTranslator(db)
@@ -24,11 +26,13 @@ async def data_translator():
             raw_data = raw_data_queue.get()
             translator.data_handler(raw_data, 'output.json', translated_data_queue)
 
+
 async def data_sender():
     await Send_Direct.begin(translated_data_queue, terminate_event)
 
+
 async def main():
-    receiver_thread = threading.Thread(target=data_receiver)
+    receiver_thread = asyncio.create_task(data_receiver())
     receiver_thread.start()
 
     translator_task = asyncio.create_task(data_translator())
