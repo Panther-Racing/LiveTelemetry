15472285ed6c16799a6f72f58920d2608173e165
Frederick Laudati <fjl11@pitt.edu>
2024-05-30 23:13:07 -0400

    Make sure json_dict is a dictionary

--- Server/Full System Redesigned 2.0/CAN_Translate.py
+++ Server/Full System Redesigned 2.0/CAN_Translate.py
@@ -89,7 +89,7 @@ class CANTranslator:
 
         if not os.path.exists(json_file_name):
             with open(json_file_name, 'w') as f:
-                json.dump([], f)
+                json.dump({}, f)
 
         with open(json_file_name, 'r+') as json_file:
 
@@ -101,10 +101,10 @@ class CANTranslator:
                     json_file.seek(0)
                     json_dict = json.load(json_file)
                 else:
-                    json_dict = {}
+                    json_dict = dict()
             except json.decoder.JSONDecodeError:
                 print("Error decoding JSON from the file. Initializing with an empty dictionary.")
-                json_dict = {}
+                json_dict = dict()
 
             # Update the JSON dictionary with the new data
             json_dict.update(decoded)
