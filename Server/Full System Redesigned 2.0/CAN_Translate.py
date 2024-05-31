1c458a36f003ab6b003f1a4ff06a92008f21cd13
Frederick Laudati <fjl11@pitt.edu>
2024-05-30 23:01:23 -0400

    Removed output monitoring file

--- Server/Full System Redesigned 2.0/CAN_Translate.py
+++ Server/Full System Redesigned 2.0/CAN_Translate.py
@@ -38,14 +38,12 @@ class CANTranslator:
             decoded = self.db.decode_message(frame_id_or_name=frame_id, data=data_reformatted, decode_choices=False, scaling=True,
                                              decode_containers=False, allow_truncated=False)
             print(f'decoded: {decoded}')
-            CANTranslator.to_json(decoded, latency / 1000, json_file_name, output_monitoring, processed_data, arduino_time)
+            CANTranslator.to_json(decoded, latency / 1000, json_file_name, processed_data, arduino_time)
 
         except KeyError as error:
             print('Key error: %s' % error)
-            output_monitoring.write('Key error: %s \n' % error)
         except ValueError as error:
             print('Value error: %s' % error)
-            output_monitoring.write('Value error: %s \n' % error)
 
     @staticmethod
     def find_nth(haystack, needle, n):
