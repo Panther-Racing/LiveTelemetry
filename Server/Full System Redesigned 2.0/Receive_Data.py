91085c57eda2e4799af86eaaa89c3bc3d9d6b925
Frederick Laudati <fjl11@pitt.edu>
2024-05-30 23:51:44 -0400

    Added debugging

--- Server/Full System Redesigned 2.0/Receive_Data.py
+++ Server/Full System Redesigned 2.0/Receive_Data.py
@@ -6,6 +6,7 @@ def begin(raw_data_queue, terminate_event):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((socket.gethostbyname(socket.gethostname()), 20001))
     s.settimeout(1)
+    print(f'Listening at {socket.gethostbyname(socket.gethostname())} on port 20001')
 
     while not terminate_event.isSet():
         try:
